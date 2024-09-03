0c88e0482e571642e546bb94d7589e80e746b354
cool <cool@DESKTOP-KPOO7FJ>
2024-09-03 14:27:57 -0700

    hi

---
+++ src/main/java/genericUtilityOrLib/Chiguru.java
@@ -0,0 +1,9 @@
+package genericUtilityOrLib;
+
+public class Chiguru {
+
+	public static void main(String[] args) {
+
+	}
+
+}
--- src/main/java/genericUtilityOrLib/IconstantUtility.java
+++ src/main/java/genericUtilityOrLib/IconstantUtility.java
@@ -2,7 +2,7 @@ package genericUtilityOrLib;
 
 public interface IconstantUtility {
 	
-	public String propertiesPath="C:\\SELENIUM\\CommonData.properties";
+	public String propertiesPath="C:\\SELENIUM\\ChiguruData.properties";
 	  public String ExcelPath = "C:\\SELENIUM\\TestCaseData.xlsx";
 
 }
